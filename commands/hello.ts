35a766da9cd292f6b42c8eed536129a286aac09d
meizuflux <76911441+meizuflux@users.noreply.github.com>
2022-01-31 21:01:06 +0000

    basic command lib

---
+++ commands/hello.ts
@@ -0,0 +1,7 @@
+import * as lib from "./_lib"
+
+
+export default new lib.Command({
+    name: "hello",
+    description: "says hello",
+})
---
+++ scripts/commands.ts
@@ -0,0 +1,7 @@
+/*
+system for adding, updating, and checking commands
+*/
+
+import hello from "../commands/hello";
+
+console.log(hello.to_json())
