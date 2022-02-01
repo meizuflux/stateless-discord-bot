42a122f98a2e887ccf4595c21435b100bd0e10aa
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2022-02-01 16:35:30 -0500

    Update framework

--- lib/api.ts
+++ lib/api.ts
@@ -9,7 +9,45 @@ class APIHandler {
     constructor(id: string, secret: string) {
         this.id = id
         this.secret = secret
-    }    
+    }
+
+    async request(method: string, path: string, body?: {}): Promise<any> {
+        this.verifyTokenExists("request()")
+
+        let options = {
+            method: method,
+            headers: {
+                Authorization: `Bearer ${this.token}`
+            }
+        }
+
+        if (body != null) {
+            options["body"] = JSON.stringify(body)
+            options.headers["Content-Type"] = "application/json"
+        }
+
+        const modifiedPath = path.replace("{id}", this.id)
+
+        const res = await fetch("https://discord.com/api/v9" + modifiedPath, options)
+
+        if (!res.ok) {
+            throw new Error(`${path} failed with code ${res.status}: ${await res.text()}`)
+        }
+
+        return await res.json()
+    }
+
+    async getGlobalCommands(): Promise<[]> {
+        this.verifyTokenExists("getGlobalCommands()")
+
+        return await this.request("GET", "/applications/{id}/commands")
+    }
+
+    verifyTokenExists(name: string) {
+        if (this.token === null) {
+            throw new Error("Please call fetchToken() before using " + name)
+        }
+    }
 
     async fetchToken(): Promise<string> {
         const res = await fetch("https://discord.com/api/oauth2/token", {
