f162eaa1f71843447e241515cb3c9721eb97282a
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2022-02-01 16:35:30 -0500

    Update framework

--- scripts/commands.ts
+++ scripts/commands.ts
@@ -1,30 +1,77 @@
-/*
-system for adding, updating, and checking commands
-*/
-
-import yargs from "yargs"
-import { hideBin } from "yargs/helpers"
 import hello from "../commands/hello.js"
 import APIHandler from "../lib/api.js"
-
-console.log(hello.to_json())
-
-import config from "../dev.config.js"
+import dev_config from "../dev.config.js"
 import prod_config from "../prod.config.js"
+let config = dev_config
 
-const args = yargs(hideBin(process.argv))
-    .option("prod", {
-        default: false,
-        type: "boolean",
-        describe: "Using this flag will use \"prod.config.ts\" instead of \"dev.config.ts\"."
-    })
-    .argv
+const args = process.argv.splice(2)
 
-if (args["prod"] === true) {
-    const config = prod_config
+// checking for flags
+for (let [i, arg] of args.entries()) {
+    // check if production flag was used, and remove it from the args, then change configuration to match
+    if (["--production", "--prod", "-p"].includes(arg.toLowerCase())) {
+        args.splice(i, 1)
+        config = prod_config
+    }
+    // help menu
+    if (["--help", "-h"].includes(arg.toLowerCase())) {
+        console.log(`Commands:
+    init - Initializes and overwrites all global commands
+    update [command] - Updates specified command
+    delete [command] - Deletes specified command
+    show - Shows 
+
+Providing no command defaults to the "show" command
+
+Flags:
+    --help (-h) - Shows this menu
+    --production (--prod, -p) - Uses "prod.config.ts" instead of "dev.config.ts"
+        `)
+    }
+}
+// default command
+if (args.length == 0) {
+    args[0] = "show"
 }
 
-const client = new APIHandler(config.id, config.secret)
+// array of valid commands with their associated
+const _commands = {
+    show: show
+}
 
-console.log(await client.fetchToken())
+// make sure command specified is a valid command
+if (!Object.keys(_commands).includes(args[0])) {
+    console.log(`Unknown command: ${args[0]}
+Try running with the "--help" flag to view all valid commands`
+    )
+    process.exit(1)
+}
+
+// setup client
+const client = new APIHandler(config.id, config.secret)
+await client.fetchToken()
+
+// get function and call it 
+const fn = _commands[args[0]]
+await fn()
+
+/*
+    COMMANDS BELOW
+*/
+
+async function show(): Promise<void> {
+    const commands = await client.getGlobalCommands()
+
+    let names = []
+
+    for (let command of commands) {
+        names.push(command["name"])
+    }
+
+    console.log(`There are ${names.length} global commands
+
+Commands:
+    ${names.join("\n")}
+    `)
+}
 
